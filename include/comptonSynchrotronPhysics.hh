c946657b2b0d01743d26ef6178155944bb068b5b
Prakash Gautam <pgautam@jlab.org>
2025-02-24 15:00:51 -0500

    Add explicit class for synchrotron Physics

---
+++ include/comptonSynchrotronPhysics.hh
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "G4DecayPhysics.hh"
+
+class comptonSynchrotronPhysics: public G4VPhysicsConstructor {
+public:
+    comptonSynchrotronPhysics(G4int verbose = 0, const G4String& name = "comptonSynchrotronPhysics");
+    virtual ~comptonSynchrotronPhysics();
+protected:
+    void ConstructParticle();
+    void ConstructProcess();
+    void ConstructEM();
+private:
+    comptonSynchrotronPhysics(const comptonSynchrotronPhysics& right) = delete;
+    comptonSynchrotronPhysics& operator=(const comptonSynchrotronPhysics& right) = delete;
+    G4bool fSRType;
+    G4DecayPhysics* fDecayPhysics;
+};
